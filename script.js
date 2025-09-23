2e0d1fb0fe650afff0617aa97ffa09db70e8e590
Final <luongnguyentruong@proton.me>
2025-09-23 17:19:09 +1000

    Added basic calculation functionality

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function calculateBoxAddress(
 	boxSlot,
 	slotOffset,
 ) {
-	const baseAddressStart = baseAddress + baseAddressOffsetStart;
-	const baseAddressEnd = baseAddress + baseAddressOffsetEnd;
+	const baseAddressStart = baseAddress + 4 + baseAddressOffsetStart;
+	const baseAddressEnd = baseAddress + 4 + baseAddressOffsetEnd;
 	const distance =
-		(TOTAL_BOX_SLOTS * (boxNumber - 1) + boxSlot)
+		(TOTAL_BOX_SLOTS * (boxNumber - 1) + (boxSlot - 1))
 		* BOX_POKEMON_SIZE
 		+ slotOffset;
 	return [
@@ -22,4 +22,35 @@ function calculateBoxAddress(
 	];
 }
 
+function handleBoxAddressCalculatorFormSubmit(event) {
+	event.preventDefault();
+
+	const formData = new FormData(event.target);
+	const baseAddress = Number(formData.get("base-address"));
+	const baseAddressOffsetStart =
+		Number(formData.get("base-address-shift-start"));
+	const baseAddressOffsetEnd =
+		Number(formData.get("base-address-shift-end"));
+	const boxNumber = Number(formData.get("box-number"));
+	const boxSlot = Number(formData.get("box-slot"));
+	const slotOffset = Number(formData.get("slot-offset"));
+
+	const result = calculateBoxAddress(
+		baseAddress,
+		baseAddressOffsetStart,
+		baseAddressOffsetEnd,
+		boxNumber,
+		boxSlot,
+		slotOffset,
+	);
+
+	const resultOutput = result[0] === result[1]
+		? `0x${result[0].toString(16)}`
+		: `0x${result[0].toString(16)} to 0x${result[1].toString(16)}`;
+	event.target.querySelector("#box-address").innerText = resultOutput;
+}
+
+document
+	.querySelector("#box-address-calculator-form")
+	.addEventListener("submit", handleBoxAddressCalculatorFormSubmit);
 })();
